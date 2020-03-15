00eb7b50c02ff8e6e5bc6ef9d908059383abad0b
Ootori008 <koushihou@hotmail.com>
2020-03-15 18:22:30 +0900

    Update url and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 # If a command fails then the deploy stops
-# set -e
+set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-rm -rf ../ootori.github.io/*
-mv public/* ../
+cd public
 
 # Add changes to git.
 git add .
@@ -23,4 +22,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-# git push master
+git push origin master
